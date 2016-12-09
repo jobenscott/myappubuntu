1fc6f0d60fd5b0b81e5c3856d2948fafe7e36b44
jobenscott <jobenscott@yahoo.com>
2016-12-09 10:21:46 -0700

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   # root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
 
-  namespace :app_proxy do
-    root action: 'index'
-    # simple routes without a specified controller will go to AppProxyController
+  # namespace :app_proxy do
+  #   root action: 'index'
+  #   # simple routes without a specified controller will go to AppProxyController
     
-    # more complex routes will go to controllers in the AppProxy namespace
-    # 	resources :reviews
-    # GET /app_proxy/reviews will now be routed to
-    # AppProxy::ReviewsController#index, for example
-  end
+  #   # more complex routes will go to controllers in the AppProxy namespace
+  #   # 	resources :reviews
+  #   # GET /app_proxy/reviews will now be routed to
+  #   # AppProxy::ReviewsController#index, for example
+  # end
 # root 'home#index'
 get 'index' => "home#index"
 
