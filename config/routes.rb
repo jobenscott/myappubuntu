2d030e21d6b78f919900ff4a980924d731165cce
jobenscott <jobenscott@yahoo.com>
2016-12-09 16:40:44 -0700

    eye of the tiger

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
   # root :to => 'home#index'
   # root :to => 'home#index'
-  get 'index' => "home#index"
+
   mount ShopifyApp::Engine, at: '/'
 
-  # namespace :app_proxy do
-    # root action: 'index'
+  namespace :app_proxy do
+    root action: 'index'
     # simple routes without a specified controller will go to AppProxyController
     
     # more complex routes will go to controllers in the AppProxy namespace
     # 	resources :reviews
     # GET /app_proxy/reviews will now be routed to
     # AppProxy::ReviewsController#index, for example
-  # end
+  end
+    get 'index' => "home#index"
 # root  'test#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
