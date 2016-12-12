c836f119679bb6ee2039fcf1b1333f39d52b6887
jobenscott <jobenscott@yahoo.com>
2016-12-12 12:21:09 -0700

    getting assets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
   end
+
+  def testIndex
+  	# @assets = ShopifyAPI::Session.temp(@shopify_session.url, @shopify_session.token) {
+  		@assets = ShopifyAPI::Asset.find(:all)
+	# }
+  end
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
-  config.scope = 'read_customers, read_orders, read_products'
+  config.scope = 'read_customers, read_orders, read_products, read_themes, write_themes'
   config.embedded_app = true
 end
