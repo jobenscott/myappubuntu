68c7ac14ffb1b768596be64e899c10e391d68b4f
jobenscott <jobenscott@yahoo.com>
2016-12-13 17:21:20 -0700

    not accepting redirect_uri as method

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
-  config.redirect_uri = "https://radiant-savannah-16136.herokuapp.com/auth/shopify/callback"
   config.scope = 'read_customers, read_orders, read_products, read_themes, write_themes'
   config.embedded_app = true
+  config.redirect_uri = "https://radiant-savannah-16136.herokuapp.com/auth/shopify/callback"
 end
