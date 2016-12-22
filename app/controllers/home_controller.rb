649db2180c48e8ea9c5e46ff0f5baf3a3de5c6fd
jobenscott <jobenscott@yahoo.com>
2016-12-22 10:55:24 -0700

    daasd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
   	# https://{shop}.myshopify.com/admin/oauth/authorize?client_id={api_key}&scope={scopes}&redirect_uri={redirect_uri}&state={nonce
-  	# @url = "https://#{@shop_session.url}/admin/oauth/access_token?client_id="https://{shop}.myshopify.com/admin/oauth/authorize?client_id={api_key}&scope={scopes}&redirect_uri={redirect_uri}&state={nonce
+  	# @url = "https://{@shopify_session.name}.myshopify.com/admin/oauth/authorize?client_id={api_key}&scope={scopes}&redirect_uri={redirect_uri}&state={nonce"
   #   session = ShopifyAPI::Session.new(self.url, self.access_token)
   	# @something = @shop_session
   	# @something2 = @shop_session.api_key
@@ -10,9 +10,9 @@ class HomeController < ShopifyApp::AuthenticatedController
     # ShopifyAPI::Base.active_session(session)
     # @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
    	# redirect_to "https://radiant-savannah-16136.herokuapp.com/getAccess"
-   	session = ShopifyAPI::Session.new(@shopify_session.url, @shopify_session.token)
-   	ShopifyAPI::Base.active_session(session)
-   	ShopifyAPI::ScriptTag.create(:event => "onload", :src => 'https://radiant-savannah-16136.herokuapp.com/assets/javascripts/test.js')
+   	# session = ShopifyAPI::Session.new(@shopify_session.name, @shopify_session.token)
+   	# ShopifyAPI::Base.active_session(session)
+   	# ShopifyAPI::ScriptTag.create(:event => "onload", :src => 'https://radiant-savannah-16136.herokuapp.com/assets/javascripts/test.js')
   end
 
   def testIndex
